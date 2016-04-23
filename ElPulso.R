0689e24cc1d829c6b43930ac0020f1339893ff47
Juan Pablo Moraga <jp.moraga.leigh@gmail.com>
2016-04-23 20:38:05 -0300

    Added files via upload

--- ElPulso.R
+++ ElPulso.R
@@ -21,6 +21,29 @@ LT <- html_nodes(Noticia,"h1")
 LT <- as.character(LT)
 LT <- strsplit(LT,"title=")
 
-####LT[[1]][1]
+N <- c(NULL,NULL,NULL)
 
+for (i in 1:4)
+  
+    {
+    Link <- LT[[i]][1]
+    Link <- strsplit(Link,'<a href=\"')
+    Link <- Link[[1]][2]
+    Link <- paste("http://www.pulso.cl",Link,sep = "")
 
+    Titulo <- LT[[i]][2]
+    Titulo <- sub(".*?>(.*?)</a>\n</h1>.*", "\\1", Titulo)
+    
+
+    N1 <- cbind(i,Link,Titulo)
+    
+    N <- rbind(N,N1)
+    
+    i = i + 1
+    
+}
+
+Resumen <- html_nodes(Noticia,"p")
+Resumen <- sub(".*?<p>(.*?)</p>.*", "\\1", Resumen)
+
+Base <- cbind(N,Resumen)
